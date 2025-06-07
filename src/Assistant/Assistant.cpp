6e5b234db20fd9e6f80b7f7906b08fa0f0e4d5f8
ItwenzyI <kischoenhaar@gmail.com>
2025-06-07 19:05:04 +0200

    Addes Assistant

--- src/Assistant/Assistant.cpp
+++ src/Assistant/Assistant.cpp
@@ -1,5 +1,20 @@
-//
-// Created by User on 06.06.2025.
-//
+
 
 #include "Assistant.hpp"
+#include <iostream>
+
+Assistant::Assistant(const std::string& firstName, const std::string& lastName) 
+    : User(firstName, lastName) {
+
+};
+
+Assistant::~Assistant() {};
+
+void Assistant::displayMenu() {
+    std::cout << "\n=== Assistant Menu ===" << std::endl;
+    std::cout << "1. View Medical Records" << std::endl;
+    std::cout << "2. View Documents" << std::endl;
+    std::cout << "3. View open Appointments" << std::endl;
+    std::cout << "4. View Calender" << std::endl;
+    std::cout << "9. Logout" << std::endl;
+}
--- src/Assistant/Assistant.hpp
+++ src/Assistant/Assistant.hpp
@@ -1,8 +1,19 @@
-//
-// Created by User on 06.06.2025.
-//
 
 #ifndef ASSISTANT_HPP
 #define ASSISTANT_HPP
+#include "../User/User.hpp"
+
+class Assistant : public User {
+    public:
+
+    Assistant(const std::string& firstName, const std::string& lastName);
+
+    //Desturctor
+    virtual ~Assistant();
+
+    void displayMenu() override;
+};
+
+
 
 #endif //ASSISTANT_HPP
