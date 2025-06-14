faf934552d8ff2d7d38c27f67425cf7789391bbb
ItwenzyI <kischoenhaar@gmail.com>
2025-06-14 13:13:17 +0200

    Added new comments for main.lua

--- main.cpp
+++ main.cpp
@@ -28,23 +28,21 @@ int main() {
     std::filesystem::create_directories("data");
     if (!std::filesystem::exists("data/patient_id.txt")) {
         std::ofstream out("data/patient_id.txt");
-        out << "1"; // Initialvalue
+        out << "1"; // Initial Value
         out.close();
     }
     if (!std::filesystem::exists("data/assistant_id.txt")) {
         std::ofstream out("data/assistant_id.txt");
-        out << "1"; // Initialvalue
+        out << "1"; // Initial Value
         out.close();
     }
     if (!std::filesystem::exists("data/doctor_id.txt")) {
         std::ofstream out("data/doctor_id.txt");
-        out << "1"; // Initialvalue
+        out << "1"; // Initial Value
         out.close();
     }
 
-    //Patient::createNewPatient("test21", "test24");
-    //Doctor::createNewDoctor("test21", "test24");
-
+    // Checks if any Folder of Patients or Doctors or Assistant is already created.
     Admin::checkInitialSetup();
 
 
