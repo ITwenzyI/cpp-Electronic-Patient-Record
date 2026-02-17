e84dfeacfe7eca44f0a269c4f87157efa6d4ad69
Kilian <kischoenhaar@gmail.com>
2026-02-17 19:47:35 +0100

    refactor(infra): implement repositories as adapters

---
+++ src/infrastructure/persistence/FileUserRepository.hpp
@@ -0,0 +1,14 @@
+#ifndef FILE_USER_REPOSITORY_HPP
+#define FILE_USER_REPOSITORY_HPP
+
+#include "application/ports/IUserRepository.hpp"
+
+class FileUserRepository final : public IUserRepository {
+public:
+    bool exists(const std::string& id) const override;
+    std::vector<std::string> readInfo(const std::string& id) const override;
+    bool writeInfo(const std::string& id, const std::vector<std::string>& lines) override;
+    bool appendInfoLine(const std::string& id, const std::string& line) override;
+};
+
+#endif //FILE_USER_REPOSITORY_HPP
