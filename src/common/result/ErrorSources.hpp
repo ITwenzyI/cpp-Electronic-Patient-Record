759cdeabbbf0e7740dec0a57f6c0d68cbb1a09ad
Kilian <kischoenhaar@gmail.com>
2026-02-18 12:34:02 +0100

    refactor(common): add shared error source constants

---
+++ src/common/result/ErrorSources.hpp
@@ -0,0 +1,10 @@
+#ifndef ERROR_SOURCES_HPP
+#define ERROR_SOURCES_HPP
+
+namespace ErrorSources {
+constexpr const char* kApplication = "app";
+constexpr const char* kUi = "ui";
+constexpr const char* kInfrastructure = "infra";
+}
+
+#endif // ERROR_SOURCES_HPP
